74c9800fe9becaf0d1d9fdf6f58a1abdda3987b9
Shiva <gopireddyssreddy@gmail.com>
2024-04-21 19:03:55 +0530

    Manager and Querysets in Django

--- django_part_1/django_ORM/Start/storefront/playground/views.py
+++ django_part_1/django_ORM/Start/storefront/playground/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-def calculate():
-    x = 1
-    y = 2
-    return x
-
+from store.models import Product
 
 def say_hello(request):
-    x = calculate()
+    # case-1 : Query_sets looping
+    # query_set = Product.objects.all()
+    # for product in query_set:
+    #     print(product)
+
+    # case-2 : Query_sets converting to list
+    # query_set = Product.objects.all()
+    # result = list(query_set)
+
+    # case-3 : specific value indexing or slicing the query_Set
+    # query_set = Product.objects.all()
+    # result =  query_set[3:5]
+
+    # case-4 : return the count()
+    query_set = Product.objects.count()
+    print(query_set)
+
+
+
+
     return render(request, 'hello.html', {'name': 'Mosh'})
