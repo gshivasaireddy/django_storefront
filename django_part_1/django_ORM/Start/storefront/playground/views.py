00cc07ff0fb24d22e9a61bd4b3e526e9bbe223c0
Shiva <gopireddyssreddy@gmail.com>
2024-04-26 18:59:17 +0530

    Retrieving objects using get(),filter() methods

--- django_part_1/django_ORM/Start/storefront/playground/views.py
+++ django_part_1/django_ORM/Start/storefront/playground/views.py
@@ -1,26 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 def say_hello(request):
-    # case-1 : Query_sets looping
-    # query_set = Product.objects.all()
-    # for product in query_set:
-    #     print(product)
 
-    # case-2 : Query_sets converting to list
-    # query_set = Product.objects.all()
-    # result = list(query_set)
+    # Retrieving methods such as get(),all(),filter()
+    # try catch incase if the object value in store_product table doesn't exist
+    # try:
+    #     product=Product.objects.get(pk=0)
+    # except ObjectDoesNotExist:
+    #     pass
 
-    # case-3 : specific value indexing or slicing the query_Set
-    # query_set = Product.objects.all()
-    # result =  query_set[3:5]
+    # Alternate way is use filter() and retrieve first(), if it doesn't find , it returns None
+    #product=Product.objects.filter(pk=0).first()
 
-    # case-4 : return the count()
-    query_set = Product.objects.count()
-    print(query_set)
-
-
-
+    # sometimes we want to check if the object exist/no
+    product=Product.objects.filter(pk=0).exists()
 
     return render(request, 'hello.html', {'name': 'Mosh'})
