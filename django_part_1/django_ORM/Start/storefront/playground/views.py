7d58c8d8cf510e229200d150942bc8ac2e90fecf
Shiva <gopireddyssreddy@gmail.com>
2024-04-29 19:25:37 +0530

    Complex Lookups using Q(query expression) objects with OR, AND,NOT

--- django_part_1/django_ORM/Start/storefront/playground/views.py
+++ django_part_1/django_ORM/Start/storefront/playground/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from store.models import Product
 
 def say_hello(request):
-    # filtering in detail
-    # 
-    #queryset =Product.objects.filter(unit_price__gt=20)
-    # queryset =Product.objects.filter(collection__id__range=(1,2,3))
-    #queryset =Product.objects.filter(last_update__year=2021)
-    queryset = Product.objects.filter(description__isnull=True)
-    
-    
+    # complex lookups using Q-objects
+    # ex-1: inventory<10 and price<20
+    # queryset=Product.objects.filter(inventory__lt=10,unit_price__lt=20)
+    # method-2
+    # queryset=Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20)
+
+    # using Q-objects
+    # Products: inventory <10 OR price<20
+    # queryset=Product.objects.filter(Q(inventory__lt=10)|Q(unit_price__lt=20))
+    queryset=Product.objects.filter(Q(inventory__lt=10) & ~Q(unit_price__lt=20))
 
     return render(request, 'hello.html', {'name': 'Mosh','products':list(queryset)})
