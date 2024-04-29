0ca8c0f104a7c6cab6424c810d3a0716c2abb6c4
Shiva <gopireddyssreddy@gmail.com>
2024-04-29 19:39:05 +0530

    Referencing and Filtering using F-objects(Field objects)

--- django_part_1/django_ORM/Start/storefront/playground/views.py
+++ django_part_1/django_ORM/Start/storefront/playground/views.py
@@ -1,19 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q
+from django.db.models import Q,F
 from store.models import Product
 
 def say_hello(request):
-    # complex lookups using Q-objects
-    # ex-1: inventory<10 and price<20
-    # queryset=Product.objects.filter(inventory__lt=10,unit_price__lt=20)
-    # method-2
-    # queryset=Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20)
-
-    # using Q-objects
-    # Products: inventory <10 OR price<20
-    # queryset=Product.objects.filter(Q(inventory__lt=10)|Q(unit_price__lt=20))
-    queryset=Product.objects.filter(Q(inventory__lt=10) & ~Q(unit_price__lt=20))
+    # F objects - used to compare fields and filter it
+    # queryset=Product.objects.filter(inventory=F('unit_price'))
+    queryset=Product.objects.filter(inventory=F('collection__id'))
 
     return render(request, 'hello.html', {'name': 'Mosh','products':list(queryset)})
