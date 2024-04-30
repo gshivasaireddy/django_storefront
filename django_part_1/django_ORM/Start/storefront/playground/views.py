3e8469da429022703ae8063044306d27f9bdfdba
Shiva <gopireddyssreddy@gmail.com>
2024-04-30 20:21:00 +0530

    Selecting Fields , subqueries using values-returns dict, values_list-returns tuples

--- django_part_1/django_ORM/Start/storefront/playground/views.py
+++ django_part_1/django_ORM/Start/storefront/playground/views.py
@@ -2,15 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q,F
-from store.models import Product
+from store.models import Product,OrderItem
 
 def say_hello(request):
-    # Limiting the results
-    queryset=Product.objects.all()[:5]
+    # selecting fields to query by using values()-> returns key:value pairs
+    queryset=Product.objects.values('id','title')
 
-    # limit 5 and offset 5
-    queryset2=Product.objects.all()[5:10]
+    # retriving related table columns as well
+    queryset2=Product.objects.values('id','title','collection__title')
 
-    
+    # values_list() --> returns tuples
+    queryset3=Product.objects.values_list('id','title','collection__title')
 
-    return render(request, 'hello.html', {'name': 'Mosh','products':list(queryset),'products2':list(queryset2)})
+    # show all the products who has orders(OrderItem table contains all the order details) and sort by title
+    queryset4=Product.objects.filter(id__in=OrderItem.objects.values('product__id').distinct()).order_by('title')
+
+    return render(request, 'hello.html', {'name': 'Mosh','products':list(queryset4),'products3':list(queryset),'products2':list(queryset2),'products4':list(queryset)},
+    )
