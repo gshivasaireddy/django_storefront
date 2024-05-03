6c988b06b231bb86e3bd5708585ea55835152b94
Shiva <gopireddyssreddy@gmail.com>
2024-05-03 18:18:19 +0530

    Deferring fields using only() and defer() methods

--- django_part_1/django_ORM/Start/storefront/playground/views.py
+++ django_part_1/django_ORM/Start/storefront/playground/views.py
@@ -5,17 +5,11 @@ from django.db.models import Q,F
 from store.models import Product,OrderItem
 
 def say_hello(request):
-    # selecting fields to query by using values()-> returns key:value pairs
-    queryset=Product.objects.values('id','title')
+    # Deferring fields : only()
+    #queryset=Product.objects.only('id','title','unit_price')
 
-    # retriving related table columns as well
-    queryset2=Product.objects.values('id','title','collection__title')
+    # Deferring fields: defer() excludes the mentioned fields
+    queryset=Product.objects.defer('description')
 
-    # values_list() --> returns tuples
-    queryset3=Product.objects.values_list('id','title','collection__title')
-
-    # show all the products who has orders(OrderItem table contains all the order details) and sort by title
-    queryset4=Product.objects.filter(id__in=OrderItem.objects.values('product__id').distinct()).order_by('title')
-
-    return render(request, 'hello.html', {'name': 'Mosh','products':list(queryset4),'products3':list(queryset),'products2':list(queryset2),'products4':list(queryset)},
+    return render(request, 'hello.html', {'name': 'Mosh','products':list(queryset)},
     )
