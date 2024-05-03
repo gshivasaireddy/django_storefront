bd465d315ee575dedc18746917a91a8230b6ccc2
Shiva <gopireddyssreddy@gmail.com>
2024-05-03 19:39:55 +0530

    Selecting Related Objects using select_related(),prefetch_related() methods

--- django_part_1/django_ORM/Start/storefront/playground/views.py
+++ django_part_1/django_ORM/Start/storefront/playground/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q,F
-from store.models import Product,OrderItem
+from store.models import Product,OrderItem,Order
 
 def say_hello(request):
-    # Deferring fields : only()
-    #queryset=Product.objects.only('id','title','unit_price')
+    # Selecting Related Objects: select_related(many to 1 or 1-1)
+    # queryset=Product.objects.select_related('collection').all()
 
-    # Deferring fields: defer() excludes the mentioned fields
-    queryset=Product.objects.defer('description')
+    # prefetch_related(many to many , one to many scenarios)
+    # queryset=Product.objects.prefetch_related('promotions').select_related('collection').all()
 
-    return render(request, 'hello.html', {'name': 'Mosh','products':list(queryset)},
+    # Exercise: Get the last 5 orders with their Customer and items (including Product)
+    queryset=Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+
+    return render(request, 'hello.html', {'name': 'Mosh','orders':list(queryset)},
     )
