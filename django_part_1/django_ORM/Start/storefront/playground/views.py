7183deb7ad57bb39dffa60e48af36ee1e026e96e
Shiva <gopireddyssreddy@gmail.com>
2024-05-04 18:41:15 +0530

    Aggregating Objects using aggregate() by passing Min(),Count() etc.,

--- django_part_1/django_ORM/Start/storefront/playground/views.py
+++ django_part_1/django_ORM/Start/storefront/playground/views.py
@@ -2,17 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q,F
+from django.db.models.aggregates import Count,Max,Min,Avg,Sum
 from store.models import Product,OrderItem,Order
 
 def say_hello(request):
-    # Selecting Related Objects: select_related(many to 1 or 1-1)
-    # queryset=Product.objects.select_related('collection').all()
+    # Aggregating Objects
+    result=Product.objects.filter(collection__id=1).aggregate(count=Count('id'),min_price=Min('unit_price'))
 
-    # prefetch_related(many to many , one to many scenarios)
-    # queryset=Product.objects.prefetch_related('promotions').select_related('collection').all()
-
-    # Exercise: Get the last 5 orders with their Customer and items (including Product)
-    queryset=Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
-
-    return render(request, 'hello.html', {'name': 'Mosh','orders':list(queryset)},
+    return render(request, 'hello.html', {'name': 'Mosh','result':result},
     )
