3c1f82c89da46574c4bbe629d5642d4473de3bd1
Shiva <gopireddyssreddy@gmail.com>
2024-05-05 17:27:50 +0530

    Querying Generic Relationships-Identify tags for a specific Product

--- django_part_1/django_ORM/Start/storefront/playground/views.py
+++ django_part_1/django_ORM/Start/storefront/playground/views.py
@@ -5,20 +5,19 @@ from django.db.models import Q,F,DecimalField
 from django.db.models.aggregates import Count,Max,Min,Avg,Sum
 from django.db.models.expressions import Value,F,Func,ExpressionWrapper
 from django.db.models.functions import Concat
+from django.contrib.contenttypes.models import ContentType
+from tags.models import TaggedItem
 from store.models import Product,OrderItem,Order,Customer
 
 def say_hello(request):
-    # Annotating Objects: Expression Wrapper
-    # Case-1: Without ExpressionWrapper->throws exception 
-    # queryset=Product.objects.annotate(
-    #     discounted_price=F('unit_price')*0.8
-    # )
+    # Querying Generic Relationships
+    content_type=ContentType.objects.get_for_model(Product) #returns the row corresponding the content_type of Product class/model
 
-    # case-2: With ExpressionWrapper
-    discounted_price=ExpressionWrapper(F('unit_price')*0.8,output_field=DecimalField())
-    queryset=Product.objects.annotate(
-        discounted_price=discounted_price
-    )
+    # filter for tagged item using content_type of Product
+    queryset=TaggedItem.objects.select_related('tag').filter(
+        content_type=content_type, # to map to Product
+        object_id=1   # pass the product_id to get its tags
+    ) # returns the list of tags for product_id=1
 
     return render(request, 'hello.html', {'name': 'Mosh','result':list(queryset)},
     )
