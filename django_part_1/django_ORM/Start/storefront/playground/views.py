8dc9bcb3cc8651c89437ece03508ef64b65d8b69
Shiva <gopireddyssreddy@gmail.com>
2024-05-05 16:58:07 +0530

    Working with ExpressionWrapper class to evaluate complex Expressions

--- django_part_1/django_ORM/Start/storefront/playground/views.py
+++ django_part_1/django_ORM/Start/storefront/playground/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q,F
+from django.db.models import Q,F,DecimalField
 from django.db.models.aggregates import Count,Max,Min,Avg,Sum
-from django.db.models.expressions import Value,F,Func
+from django.db.models.expressions import Value,F,Func,ExpressionWrapper
 from django.db.models.functions import Concat
 from store.models import Product,OrderItem,Order,Customer
 
 def say_hello(request):
-    # Annotating Objects
-    # Grouping objects
-    queryset=Customer.objects.annotate(
-        orders_count=Count('order')
+    # Annotating Objects: Expression Wrapper
+    # Case-1: Without ExpressionWrapper->throws exception 
+    # queryset=Product.objects.annotate(
+    #     discounted_price=F('unit_price')*0.8
+    # )
+
+    # case-2: With ExpressionWrapper
+    discounted_price=ExpressionWrapper(F('unit_price')*0.8,output_field=DecimalField())
+    queryset=Product.objects.annotate(
+        discounted_price=discounted_price
     )
 
     return render(request, 'hello.html', {'name': 'Mosh','result':list(queryset)},
