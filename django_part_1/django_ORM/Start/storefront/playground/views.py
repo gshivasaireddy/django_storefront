ec50dcd93852e60e19a196253a474dbdc39d0031
Shiva <gopireddyssreddy@gmail.com>
2024-05-10 17:56:31 +0530

    Creating Objects to INSERT data into tables using save(),create() methods

--- django_part_1/django_ORM/Start/storefront/playground/views.py
+++ django_part_1/django_ORM/Start/storefront/playground/views.py
@@ -7,13 +7,25 @@ from django.db.models.expressions import Value,F,Func,ExpressionWrapper
 from django.db.models.functions import Concat
 from django.contrib.contenttypes.models import ContentType
 from tags.models import TaggedItem
-from store.models import Product,OrderItem,Order,Customer
+from store.models import Product,OrderItem,Order,Customer,Collection
 
 def say_hello(request):
-    # Queryset caching example
-    queryset=Product.objects.all()
-    queryset[0] # indexing queryset first
-    list(queryset)
+    # Creating objects 
+    collection = Collection()
+    # method-1 to set the value
+    collection.title='Video Games'
+    collection.featured_product_id=1
+    
+    # method-2 to set the value
+    # collection.featured_product=Product(pk=1)
+    
+    
+    # in both the cases the product with value=1 should exist
+    # save() method inserts the data into DB
+    collection.save()
+
+    # alternate way of doing using create()
+    #collection=Collection.objects.create(title='a',featured_product_id=1)
     
 
     return render(request, 'hello.html', {'name': 'Mosh'},
