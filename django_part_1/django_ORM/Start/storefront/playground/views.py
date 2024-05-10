8780e7154e701aa7495197f8eca220612416e82c
Shiva <gopireddyssreddy@gmail.com>
2024-05-10 18:40:49 +0530

    Working to Transactions to check the atomicity

--- django_part_1/django_ORM/Start/storefront/playground/views.py
+++ django_part_1/django_ORM/Start/storefront/playground/views.py
@@ -6,16 +6,26 @@ from django.db.models.aggregates import Count,Max,Min,Avg,Sum
 from django.db.models.expressions import Value,F,Func,ExpressionWrapper
 from django.db.models.functions import Concat
 from django.contrib.contenttypes.models import ContentType
+from django.db import transaction
 from tags.models import TaggedItem
 from store.models import Product,OrderItem,Order,Customer,Collection
 
+# @transaction.atomic() --> use it as decorator to makes the whole fn atomic
 def say_hello(request):
-    # Deleting objects
-    collection=Collection(pk=11)
-    collection.delete()
+    # ...
 
-    # alternate way delete entire queryset
-    Collection.objects.filter(id__gt=5).delete()
+    # Transactions
+    with transaction.atomic():
+        order=Order()
+        order.customer_id=1
+        order.save()
+
+        item=OrderItem()
+        item.order=order
+        item.product_id=-1 # product_id=-1 doesn't exist it throws integrity issue
+        item.quantity=1
+        item.unit_price=10
+        item.save()
 
     return render(request, 'hello.html', {'name': 'Mosh'},
     )
