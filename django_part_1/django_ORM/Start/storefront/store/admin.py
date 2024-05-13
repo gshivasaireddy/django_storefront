b651a867a326866057e072c24f7a9783b9ee2f7d
Shiva <gopireddyssreddy@gmail.com>
2024-05-13 17:25:04 +0530

    Adding Computed columns from the existing fields 'inventory_status'

--- django_part_1/django_ORM/Start/storefront/store/admin.py
+++ django_part_1/django_ORM/Start/storefront/store/admin.py
@@ -1,19 +1,21 @@
 from django.contrib import admin
 from . import models
 
+# Register your models here.
 # this class helps how we wanna view product on admin page
 @admin.register(models.Product) # method-1
 class ProductAdmin(admin.ModelAdmin):
-  list_display=['title','unit_price']
-
-  # fields that can be editable
+  list_display=['title','unit_price','inventory_status']
   list_editable=['unit_price']
-
-  # list per page
   list_per_page=10
 
-# let's register the products app
-#admin.site.register(models.Product,ProductAdmin) # method-2
+  @admin.display(ordering='inventory')
+  def inventory_status(self,product):
+    if product.inventory<10:
+      return 'LOW'
+    else:
+      return 'OK'
+
 
 # customization of Customer model
 @admin.register(models.Customer)
@@ -22,7 +24,8 @@ class CustomerAdmin(admin.ModelAdmin):
   list_editable=['membership']
   ordering=['first_name','last_name']
   list_per_page=10
-# Register your models here.
+
+
 # let's register our Collection app
 admin.site.register(models.Collection)
 
