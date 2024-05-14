d97265c3fb591dc67ad26197fee95c4dc7eb92d2
Shiva <gopireddyssreddy@gmail.com>
2024-05-14 18:35:52 +0530

    Selecting Related Objects from admin.py

--- django_part_1/django_ORM/Start/storefront/store/admin.py
+++ django_part_1/django_ORM/Start/storefront/store/admin.py
@@ -5,9 +5,13 @@ from . import models
 # this class helps how we wanna view product on admin page
 @admin.register(models.Product) # method-1
 class ProductAdmin(admin.ModelAdmin):
-  list_display=['title','unit_price','inventory_status']
+  list_display=['title','unit_price','inventory_status','collection_title'] # selecting related obj -collection
   list_editable=['unit_price']
   list_per_page=10
+  list_select_related=['collection']
+
+  def collection_title(self,product):
+    return product.collection.title  # related obj 
 
   @admin.display(ordering='inventory')
   def inventory_status(self,product):
@@ -24,6 +28,14 @@ class CustomerAdmin(admin.ModelAdmin):
   list_editable=['membership']
   ordering=['first_name','last_name']
   list_per_page=10
+
+
+# Let's setup the Order admin page
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+  list_display=['id','placed_at','customer']
+
+
 
 
 # let's register our Collection app
--- django_part_1/django_ORM/Start/storefront/store/models.py
+++ django_part_1/django_ORM/Start/storefront/store/models.py
@@ -57,10 +57,10 @@ class Customer(models.Model):
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
     def __str__(self):
-        return self.first_name
+        return f'{self.first_name} {self.last_name}'
 
     class Meta:
-        ordering=['first_name']
+        ordering=['first_name','last_name']
 
 
 class Order(models.Model):
