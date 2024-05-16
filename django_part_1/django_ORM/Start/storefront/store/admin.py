75e0dd75bd0c9e8de2ebc408bde0ea11abf94466
Shiva <gopireddyssreddy@gmail.com>
2024-05-16 17:57:22 +0530

    Providing links to other pages

--- django_part_1/django_ORM/Start/storefront/store/admin.py
+++ django_part_1/django_ORM/Start/storefront/store/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.db.models.aggregates import Count
+from django.utils.html import format_html,urlencode
+from django.urls import reverse
 from . import models
 
 # Register your models here.
@@ -25,11 +27,29 @@ class ProductAdmin(admin.ModelAdmin):
 # customization of Customer model
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-  list_display=['first_name','last_name','membership']
+  list_display=['first_name','last_name','membership','orders']
   list_editable=['membership']
   ordering=['first_name','last_name']
   list_per_page=10
 
+  @admin.display(ordering='orders_count')
+  def orders(self,customer):
+    url = (
+      reverse('admin:store_order_changelist')
+      + '?'
+      + urlencode(
+        {
+          'customer__id':str(customer.id)
+        }
+      )
+    )
+
+    return format_html('<a href="{}">{} Orders</a>',url,customer.orders_count)
+
+  def get_queryset(self,request):
+    return super().get_queryset(request).annotate(
+      orders_count=Count('order')
+    )
 
 # Let's setup the Order admin page
 @admin.register(models.Order)
@@ -44,10 +64,20 @@ class CollectionAdmin(admin.ModelAdmin):
 
   @admin.display(ordering='products_count')
   def products_count(self,collection):
-    return collection.products_count
+    # reference --> reverse('admin:appname_modelname_page')
+    # app:store,model:product,page:changelist
+    url = (reverse('admin:store_product_changelist') # target url
+    +'?'  # beginning of query
+    + urlencode({
+      'collection__id':str(collection.id)
+    }))  # generate querystring parameter
+    return format_html('<a href="{}">{}</a>',url,collection.products_count)
+     
 
   def get_queryset(self,request):
     return super().get_queryset(request).annotate(
       products_count=Count('product')
     )
+
+
 
